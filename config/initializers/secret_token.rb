c49fdc7dcb7248c9ec34bdebf6178f62282f751c
Melissa Carbone <mcarbone1@gmail.com>
2013-12-18 15:35:56 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ArtShoppe::Application.config.secret_key_base = '280a738c789db82893ef433912902ef1e089141f4ec1e0806e4a4eb14de81d3155a6546c325140c9ef5e02a4b6c6be2a3298c524467424711e22eed260cd71c8'
