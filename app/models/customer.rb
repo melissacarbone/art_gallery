175648273f231d6fcba4e51872b31db00a0d5d88
Melissa Carbone <mcarbone1@gmail.com>
2013-12-18 17:43:46 -0500

    tests passing for customer name and email validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,6 @@
 class Customer < ActiveRecord::Base
   has_many :sales
+
+  validates :email, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
+  validates_presence_of :name
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,9 @@
 require 'spec_helper'
 
 describe Customer do
+  it { should have_valid(:email).when('mcarbone@gmail.com', 'p.dornfeld@aol.com')}
+  it { should_not have_valid(:email).when('fail', 123)}
+
+  it { should validate_presence_of :name }
 
 end
