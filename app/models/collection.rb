b56e4d5d41b659777ec61699a45dfc38aa50ef19
Melissa Carbone <mcarbone1@gmail.com>
2013-12-19 12:01:05 -0500

    add validation test for uniquness in collection

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,6 @@
 class Collection < ActiveRecord::Base
 
   has_many :favorite_collections
+
+  validates_uniqueness_of :name
 end
--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -2,4 +2,6 @@ require "spec_helper"
 
 describe Collection do
   it { should have_many(:favorite_collections) }
+
+  it { should validate_uniqueness_of(:name) }
 end
